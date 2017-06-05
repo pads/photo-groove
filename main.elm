48ad1cadafb9a6ed4ec21b68503bf45f7c819ef0
Ben Paddock <pads@thisispads.me.uk>
2017-06-05 21:44:28 +0100

    Add selected thumbnail

--- main.elm
+++ main.elm
@@ -4,16 +4,40 @@ import Html exposing (div, h1, img, text)
 import Html.Attributes exposing (..)
 
 
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/2.jpeg" ] []
-            , img [ src "http://elm-in-action.com/3.jpeg" ] []
+            (List.map (viewThumbnail model.selectedUrl) model.photos)
+        , img
+            [ src (urlPrefix ++ "large/" ++ model.selectedUrl)
+            , class "large"
             ]
+            []
         ]
 
 
+viewThumbnail selectedUrl thumbnail =
+    img
+        [ src (urlPrefix ++ thumbnail.url)
+        , classList [ ( "selected", selectedUrl == thumbnail.url ) ]
+        ]
+        []
+
+
+initialModel =
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    }
+
+
 main =
-    view "no model yet"
+    view initialModel
