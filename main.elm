ead4df046e1efac1a0739698686f1d5137253dcd
Ben Paddock <pads@thisispads.me.uk>
2017-06-12 20:28:55 +0100

    Add type annotations

--- main.elm
+++ main.elm
@@ -1,19 +1,37 @@
 module Main exposing (..)
 
-import Html exposing (div, h1, img, text)
+import Array exposing (Array)
+import Html exposing (Html, div, h1, img, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 
 
+type alias Message =
+  { operation: String, data: String }
+
+
+type alias Photo =
+    { url : String }
+
+
+type alias Model =
+    { photos : List Photo
+    , selectedUrl : String
+    }
+
+
+urlPrefix : String
 urlPrefix =
     "http://elm-in-action.com/"
 
+update : Message -> Model -> Model
 update message model =
-  if message.operation == "SELECT_PHOTO" then
-    { model | selectedUrl = message.data }
-  else
-    model
+    if message.operation == "SELECT_PHOTO" then
+        { model | selectedUrl = message.data }
+    else
+        model
 
+view: Model -> Html Message
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
@@ -26,7 +44,7 @@ view model =
             []
         ]
 
-
+viewThumbnail : String -> Photo -> Html Message
 viewThumbnail selectedUrl thumbnail =
     img
         [ src (urlPrefix ++ thumbnail.url)
@@ -36,6 +54,7 @@ viewThumbnail selectedUrl thumbnail =
         []
 
 
+initialModel : Model
 initialModel =
     { photos =
         [ { url = "1.jpeg" }
@@ -46,5 +65,10 @@ initialModel =
     }
 
 
+photoArray : Array Photo
+photoArray =
+    Array.fromList initialModel.photos
+
+
 main =
-    Html.beginnerProgram { model = initialModel , view = view, update = update }
+    Html.beginnerProgram { model = initialModel, view = view, update = update }
