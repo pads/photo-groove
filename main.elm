5f0558491b37c8f6493d6595ea2ae97d87fb7537
Ben Paddock <pads@thisispads.me.uk>
2017-06-19 20:29:26 +0100

    Add comments

--- main.elm
+++ main.elm
@@ -1,32 +1,52 @@
+-- Exports every global function.
+
+
 module Main exposing (..)
+
+-- Import the Array type from the Array module (avoids using Array.Array).
 
 import Array exposing (Array)
 import Html exposing (Html, button, div, h1, h3, label, img, input, p, text)
+
+
+-- Import a specific list of functions. type is a reserved keyword hence the underscore.
+
 import Html.Attributes exposing (id, class, classList, src, name, type_, title)
 import Html.Events exposing (onClick)
 import Http
 import Json.Decode exposing (Decoder, string, int, list)
 import Json.Decode.Pipeline exposing (decode, required, optional)
+
+
+-- Import the Random module as is.
+
 import Random
 
 
+-- Assign a type to a specific kind of record.
+
+
 type alias Photo =
-    { url : String
+    { url : String -- record key and it's type.
     , size : Int
     , title : String
     }
 
 
 type alias Model =
-    { photos : List Photo
-    , selectedUrl : Maybe String
+    { photos : List Photo -- Photos can be a List where all values must be Photo records.
+    , selectedUrl : Maybe String -- Maybe is a special type that says it an be Type X or Nothing.
     , loadingError : Maybe String
     , chosenSize : ThumbnailSize
     }
 
 
+
+-- A union type - a type that can be represented as any of these types
+
+
 type Message
-    = SelectByUrl String
+    = SelectByUrl String -- Here, String is an argument type.
     | SelectByIndex Int
     | SurpriseMe
     | SetSize ThumbnailSize
@@ -39,6 +59,10 @@ type ThumbnailSize
     | Large
 
 
+
+-- The most simple function, takes no arguments and returns a string.
+
+
 urlPrefix : String
 urlPrefix =
     "http://elm-in-action.com/"
@@ -47,25 +71,38 @@ urlPrefix =
 photoDecoder : Decoder Photo
 photoDecoder =
     decode Photo
+        -- pipes are syntatic sugar for applying multiple functions at once.
         |> required "url" string
         |> required "size" int
         |> optional "title" string "(untitled)"
+
+
+
+{- This type definition says:
+   Given a message, return a function that takes a model.
+   That function takes a model and returns a model and command function
+   (with message argument) tuple.
+-}
 
 
 update : Message -> Model -> ( Model, Cmd Message )
 update message model =
     case message of
         SelectByUrl url ->
+            -- Create a new model and assign the url, send no command.
             ( { model | selectedUrl = Just url }, Cmd.none )
 
         SelectByIndex index ->
             let
+                -- This scopes functions to only exist inside this case statement.
                 newSelectedUrl : Maybe String
                 newSelectedUrl =
                     model.photos
                         |> Array.fromList
                         |> Array.get index
                         |> Maybe.map .url
+
+                -- This is run first and is the same as: (\photo -> photo.url)
             in
                 ( { model | selectedUrl = newSelectedUrl }, Cmd.none )
 
@@ -75,11 +112,15 @@ update message model =
                 randomPhotoPicker =
                     Random.int 0 (List.length model.photos - 1)
             in
+                -- Random.generate is the command and SelectbyIndex is the message.
                 ( model, Random.generate SelectByIndex randomPhotoPicker )
 
         SetSize size ->
             ( { model | chosenSize = size }, Cmd.none )
 
+        {- Because LoadPhotos expects a Result type argument you can split it
+           out by result (response first then error)
+        -}
         LoadPhotos (Ok photos) ->
             ( { model
                 | photos = photos
@@ -88,6 +129,7 @@ update message model =
             , Cmd.none
             )
 
+        -- Underscore denotes that we don't care about the error type here (e.g. Timeout)
         LoadPhotos (Err _) ->
             ( { model | loadingError = Just "Error!" }, Cmd.none )
 
@@ -135,7 +177,7 @@ viewThumbnail : Maybe String -> Photo -> Html Message
 viewThumbnail selectedUrl thumbnail =
     img
         [ src (urlPrefix ++ thumbnail.url)
-        , title (thumbnail.title ++ " [" ++ toString thumbnail.size  ++ " KB]")
+        , title (thumbnail.title ++ " [" ++ toString thumbnail.size ++ " KB]")
         , classList [ ( "selected", selectedUrl == Just thumbnail.url ) ]
         , onClick (SelectByUrl thumbnail.url)
         ]
@@ -172,11 +214,19 @@ initialModel =
     }
 
 
+
+-- Run once, on start up only.
+
+
 initialCommand : Cmd Message
 initialCommand =
     list photoDecoder
         |> Http.get "http://elm-in-action.com/photos/list.json"
         |> Http.send LoadPhotos
+
+
+
+-- Our entry point into the app and is referred to in index.html
 
 
 main : Program Never Model Message
@@ -185,5 +235,5 @@ main =
         { init = ( initialModel, initialCommand )
         , view = viewOrError
         , update = update
-        , subscriptions = (\_ -> Sub.none)
+        , subscriptions = (\_ -> Sub.none) -- An example of an anonymous function with an unused argument.
         }
